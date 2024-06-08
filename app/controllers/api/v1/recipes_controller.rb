a58966dcdda324e97303c125d173d9dfaaa6cec5
Grant Davis <grantdavis303@gmail.com>
2024-06-08 10:28:49 -0600

    Update Recipes Controller with Edamam API query

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,17 +1,18 @@
 class Api::V1::RecipesController < ApplicationController
   def index
     if params[:country].empty?
-      puts 'The query was empty'
       rest_countries_facade = RestCountriesFacade.new
-      all_countries_list = rest_countries_facade.get_list_of_all_countries # 250 length
+      all_countries_list = rest_countries_facade.get_list_of_all_countries
       searched_for_country = all_countries_list.sample
     else
-      puts 'The query was not empty'
       searched_for_country = params[:country]
     end
 
-    puts searched_for_country
+    edamam_facade = EdamamFacade.new
+    all_recipes = edamam_facade.get_recipes(searched_for_country)
 
-    # Hit the Edamam Recipe API to search for recipes using the searched_for_country
+    # Is it possible to set the default value of id to null/nil with the JSON API Serializer?
+    # render json: RecipeSerializer.new(all_recipes), status: 200
+    render json: RecipeSerializer.new.serialized_json(all_recipes), status: 200
   end
 end

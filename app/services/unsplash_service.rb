f8888bbe2fa6de302a0a6d569f91f13a5b4ef510
Grant Davis <grantdavis303@gmail.com>
2024-06-08 19:59:50 -0600

    Create Unsplash Service and Test

---
+++ app/services/unsplash_service.rb
@@ -0,0 +1,20 @@
+class UnsplashService
+  def get_images_from_query(searched_for_country)
+    get_url("/search/photos?query=#{searched_for_country}")
+  end
+  
+  def get_url(url)
+    response = api_connection.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+  
+  def api_connection
+    Faraday.new(
+      url: 'https://api.unsplash.com',
+      params: {
+        client_id: Rails.application.credentials.unsplash[:key]
+      },
+      headers: {'Content-Type' => 'application/json'}
+    )
+  end
+end

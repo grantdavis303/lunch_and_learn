0c6ebdac4c6320e94436a5ef44b9d53678ae296b
Grant Davis <grantdavis303@gmail.com>
2024-06-09 13:18:00 -0600

    Add User Route

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :recipes, only: [:index]
       resources :learning_resources, only: [:index]
+      resources :recipes, only: [:index]
+      resources :users, only: [:create]
     end
   end
 end

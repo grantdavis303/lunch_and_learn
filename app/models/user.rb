e98fff9725cad7902f1d05ab80c57bf2956ceb76
Grant Davis <grantdavis303@gmail.com>
2024-06-09 13:17:15 -0600

    Create User Model and Validation Tests

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  validates_presence_of :name,
+                        :email,
+                        :api_key,
+                        :password_digest
+
+  validates_uniqueness_of :email
+
+  has_secure_password
+end

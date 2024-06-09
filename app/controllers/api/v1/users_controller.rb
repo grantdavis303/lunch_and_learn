512550d5044dbc254ad420f8141844de30c0dab5
Grant Davis <grantdavis303@gmail.com>
2024-06-09 13:18:38 -0600

    Create Users Controller

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,21 @@
+class Api::V1::UsersController < ApplicationController
+  def create
+    if params[:password] == params[:password_confirmation]
+      user = User.create(
+        name: params[:name],
+        email: params[:email],
+        api_key: SecureRandom.base64.tr('+/=', 'Qrt'),
+        password: params[:password],
+        password_confirmation: params[:password_confirmation]
+      )
+
+      if !user.errors.blank?
+        render json: { "error": "#{user.errors.full_messages.to_sentence}" }, status: 400
+      elsif user.authenticate(params[:password])
+        render json: UserSerializer.new.serialized_json(user), status: 201
+      end
+    else
+      render json: { "error": "These passwords don't match" }, status: 400
+    end
+  end
+end

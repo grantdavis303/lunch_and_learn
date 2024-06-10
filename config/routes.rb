49b8698c87e8fdd17cd4def526bf0b48c38ff524
Grant Davis <grantdavis303@gmail.com>
2024-06-09 21:38:58 -0600

    Create Routes for Sessions and Favorites

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      resources :favorites, only: [:create]
       resources :learning_resources, only: [:index]
       resources :recipes, only: [:index]
+      resources :sessions, only: [:create]
       resources :users, only: [:create]
     end
   end

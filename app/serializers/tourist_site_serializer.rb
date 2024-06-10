b4bd40ef42599771a2bf019cd9450ec9e56e53fb
Grant Davis <grantdavis303@gmail.com>
2024-06-10 10:05:25 -0600

    Complete Tourist Site Serializer

--- app/serializers/tourist_site_serializer.rb
+++ app/serializers/tourist_site_serializer.rb
@@ -1,18 +1,17 @@
 class TouristSiteSerializer
   def serialized_json(top_10_tourist_sites)
-    # {
-    #   "data": #all_recipes.map do |recipe|
-    #     {
-    #       "id": nil,
-    #       "type": "recipe",
-    #       "attributes": {
-    #         "title": "#{recipe.title}",
-    #         "url": "#{recipe.url}",
-    #         "country": "#{recipe.country}",
-    #         "image": "#{recipe.image}"
-    #       }
-    #     }
-    #   end
-    # }
+    {
+      "data": top_10_tourist_sites.map do |site|
+        {
+          "id": nil,
+          "type": "tourist_site",
+          "attributes": {
+            "name": "#{site.name}",
+            "address": "#{site.address}",
+            "place_id": "#{site.place_id}"
+          }
+        }
+      end
+    }
   end
 end

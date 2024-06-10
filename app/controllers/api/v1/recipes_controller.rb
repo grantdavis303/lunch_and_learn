4aa9b2cfe3fc082cc59d3a935e450a994476e929
Grant Davis <grantdavis303@gmail.com>
2024-06-10 16:52:18 -0600

    Reformat Recipes Controller

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,9 +1,10 @@
 class Api::V1::RecipesController < ApplicationController
   def index
     if params[:country].empty?
-      rest_countries_facade = RestCountriesFacade.new
-      all_countries_list = rest_countries_facade.get_list_of_all_countries
-      searched_for_country = all_countries_list.sample
+      return render json: { "data": [] }, status: 200
+      # rest_countries_facade = RestCountriesFacade.new
+      # all_countries_list = rest_countries_facade.get_list_of_all_countries
+      # searched_for_country = all_countries_list.sample
     else
       searched_for_country = params[:country]
     end

3b1f7a7754d2753c53548cfbb417b7768420a259
Grant Davis <grantdavis303@gmail.com>
2024-06-10 10:59:46 -0600

    Formatting Fix for TouristSites Controller

--- app/controllers/api/v1/tourist_sites_controller.rb
+++ app/controllers/api/v1/tourist_sites_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::TouristSitesController < ApplicationController
   def index
     geoapify_facade = GeoapifyFacade.new
     top_10_tourist_sites = geoapify_facade.get_coordinates(params[:country])
-    
+
     render json: TouristSiteSerializer.new.serialized_json(top_10_tourist_sites), status: 200
   end
 end

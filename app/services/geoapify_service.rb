85f39b2c8b88c7cfd6cd6763c50dfeb25c5f3671
Grant Davis <grantdavis303@gmail.com>
2024-06-10 11:52:25 -0600

    Write message to clear up any confusion re: lat and lon

--- app/services/geoapify_service.rb
+++ app/services/geoapify_service.rb
@@ -4,7 +4,7 @@ class GeoapifyService
   end
 
   def get_tourist_sites_from_coordinates(latitude, longitude)
-    get_url("/v2/places?&categories=tourism&bias=proximity:#{longitude},#{latitude}")
+    get_url("/v2/places?&categories=tourism&bias=proximity:#{longitude},#{latitude}") # It looks like these are flipped but it's just the way the Geoapify orders them in the URL
   end
 
   def get_url(url)

f60756f8dc217f2ab7977ffea24befcbb260c303
Grant Davis <grantdavis303@gmail.com>
2024-06-10 10:05:09 -0600

    Complete TouristSites Controller (temporarily)

--- app/controllers/api/v1/tourist_sites_controller.rb
+++ app/controllers/api/v1/tourist_sites_controller.rb
@@ -1,10 +1,8 @@
 class Api::V1::TouristSitesController < ApplicationController
   def index
-    #binding.pry
-    #country_searched_for = params[:country]
-
-    # Get the lat and lon for the country searched for
     geoapify_facade = GeoapifyFacade.new
-    results = geoapify_facade.get_coordinates(params[:country])
+    top_10_tourist_sites = geoapify_facade.get_coordinates(params[:country])
+    
+    render json: TouristSiteSerializer.new.serialized_json(top_10_tourist_sites), status: 200
   end
 end

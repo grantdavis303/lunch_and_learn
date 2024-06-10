1dfa243fb00c310306e01550bfbd824bcddcec79
Grant Davis <grantdavis303@gmail.com>
2024-06-10 11:25:51 -0600

    Update Geoapify Test w/ New Helper Method

--- app/facades/geoapify_facade.rb
+++ app/facades/geoapify_facade.rb
@@ -3,10 +3,11 @@ class GeoapifyFacade
     service = GeoapifyService.new
     response = service.get_coordinates_from_query(searched_for_country)
     sites = service.get_tourist_sites_from_coordinates(response[:results][0][:lat], response[:results][0][:lon])
-    top_10_results = sites[:features][0...10]
+    create_poros_for_tourist_sites(sites[:features][0...10])
+  end
 
-    # Possibly refactor thisinto its own method
-    all_tourist_sites = top_10_results.map do |result|
+  def create_poros_for_tourist_sites(all_tourist_sites)
+    all_tourist_sites.map do |result|
       TouristSite.new({
         name: result[:properties][:name],
         address: result[:properties][:formatted],

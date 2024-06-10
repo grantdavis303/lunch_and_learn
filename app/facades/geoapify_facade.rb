ced6a59bafd219a7bc41d4a3ad66f0e36d756839
Grant Davis <grantdavis303@gmail.com>
2024-06-10 09:55:37 -0600

    Complete Geoapify Facade (temporarily)

--- app/facades/geoapify_facade.rb
+++ app/facades/geoapify_facade.rb
@@ -2,12 +2,16 @@ class GeoapifyFacade
   def get_coordinates(searched_for_country)
     service = GeoapifyService.new
     response = service.get_coordinates_from_query(searched_for_country)
-    # coordinates = {
-    #   latitude: response[:results][0][:lat],
-    #   longitude: response[:results][0][:lon]
-    # }
-    # binding.pry
     sites = service.get_tourist_sites_from_coordinates(response[:results][0][:lat], response[:results][0][:lon])
+    top_10_results = sites[:features][0...10]
 
+    # Possibly refactor thisinto its own method
+    all_tourist_sites = top_10_results.map do |result|
+      TouristSite.new({
+        name: result[:properties][:name],
+        address: result[:properties][:formatted],
+        place_id: result[:properties][:place_id]
+      })
+    end
   end
 end

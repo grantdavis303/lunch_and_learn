429983fe3047d1d5a80aadecfbc5e8c60a576243
Grant Davis <grantdavis303@gmail.com>
2024-06-11 09:29:13 -0600

    Create Favorites Index Action

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -1,4 +1,12 @@
 class Api::V1::FavoritesController < ApplicationController
+  def index
+    if user = User.find_by(api_key: params[:api_key])
+      render json: FavoriteSerializer.new.serialized_json(user.favorites), status: 200
+    else
+      render json: { "error": "No user found with that key" }, status: 400
+    end
+  end
+
   def create
     if user = User.find_by(api_key: params[:api_key])
       new_favorite = user.favorites.create!(
